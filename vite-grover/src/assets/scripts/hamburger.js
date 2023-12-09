987300cd5a2da946b076f53bbd1a5fc7858a32bf
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2023-12-09 03:22:01 +0300

    Responsiveness

--- vite-grover/src/assets/scripts/hamburger.js
+++ vite-grover/src/assets/scripts/hamburger.js
@@ -1,5 +1,21 @@
-import "../css/hero-section.css";
-const navigatorMenu = document.querySelector(".hero-navigator-menu")
-const hamburgerBtn = document.querySelector("#hamburger-btn")
+import "../css/hero-section.css"
+const navigatorMenu = document.querySelector(".hero-navigator-menu");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const isOnMobile = window.innerWidth <= 768;
+
+console.log(navigatorMenu,hamburgerBtn)
 
 
+if (isOnMobile) {
+  /*hamburgerBtn.style.cursor = "pointer";*/
+  hamburgerBtn.addEventListener("click", function () {
+    navigatorMenu.classList.toggle("active");
+    console.log("activated")
+  });
+}
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+
+  );
+*/

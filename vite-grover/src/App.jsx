a8fedfd4f37a72e4440f23d0fc55dec87ece8cbe
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2023-12-17 00:04:59 +0300

    [AY-2] - Layout

--- vite-grover/src/App.jsx
+++ vite-grover/src/App.jsx
@@ -32,6 +32,61 @@ function App() {
             </div>
           </nav>
         </header>
+        <section className="hero-section">
+          <div className="hero-content">
+            <div className="more-than-faster">
+              <p className="more-than-faster-title">More than Faster</p>
+              <img className="more-than-faster-image" src={images.strawberry} alt="strawberry" />
+            </div>
+            <h1 className="hero-content-title">
+              <span>Groceries</span>
+              <span>delivered in as</span>
+              <div className="content-title-container">
+                <span>little as</span>
+                <span> 2 hours</span>
+              </div>
+
+            </h1>
+            <p className="hero-content-description">
+              <span>Grocen atssures fresh grocery every morning to your</span>
+              <span>family without getting out in this pandemic.</span>
+            </p>
+             <div className="hero-content-buttons">
+              <button>Order Now</button>
+              <div className="hero-order-process">
+                <img src={images.buttonPlay} alt="play"/>
+                <span>Order Process</span>
+              </div>
+             </div>
+          </div>
+          <div className="hero-image">
+            <div className="hero-image-card">
+              <img className="orange-image" src={images.orange} alt="oranges" />
+              <div className="card-info">
+                <div className="card-info-title">
+                  <p>Fresh Orange</p>
+                  <div className="rectangle"></div>
+                </div>
+                <div className="card-info-price">
+                  <h2>$44.60</h2>
+                  <p>Free Shipping</p>
+                </div>
+              </div>
+              <img className="shopping-image" src={images.shoppingIcon} alt="" />
+            </div>
+
+            <img src={images.chef1} alt="Chef with vegetables" />
+            
+            <div className="hero-image-delivary">
+              <img src={images.alarmClockIcon} alt="clock" />
+              <div className="delivary-title">
+                <h3>Delivary</h3>
+                <p>30 Min</p>
+              </div>
+              <img className="pointer" src={images.arrow} alt="" />
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
--- vite-grover/src/image-paths.js
+++ vite-grover/src/image-paths.js
@@ -1,11 +1,32 @@
+         /*Logos*/ 
 import groverLogo from './assets/images/grover-logo.svg';
+import buttonPlay from './assets/images/button-play.png'; /* Used PNG for better result. */
+import strawberry from './assets/images/strawberry.png'; /* Used PNG for better result. */
+import orange from './assets/images/orange.svg';
+import arrow from './assets/images/arrow.svg';
+import chef1 from "./assets/images/chef1.svg";
+import chef2 from "./assets/images/chef2.svg";
+
+
+         /* Icons */
 import shoppingIcon from './assets/images/icons/shopping-icon.svg';
 import searchIcon from './assets/images/icons/search-icon.svg';
 import shoppingBagIcon from './assets/images/icons/shopping-bag-icon.svg';
+import alarmClockIcon from './assets/images/icons/alarm-clock-icon.svg';
 
 export {
+       /* Logos */
     groverLogo,
+    buttonPlay,
+    strawberry,
+    orange,
+    arrow,
+    chef1,
+    chef2,
+
+       /* Icons */
     shoppingIcon,
     searchIcon,
-    shoppingBagIcon
+    shoppingBagIcon,
+    alarmClockIcon
 };

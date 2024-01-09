5c89c59440121c9781586731029bc3a897c1ca63
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-01-09 19:22:29 +0300

    [AY-4] - Setup & Updated before codes

---
+++ vite-grover/src/components/servicesSection/services.jsx
@@ -0,0 +1,12 @@
+import { shoppingBagIcon, groverLogo, searchIcon } from "../../image-paths.js";
+import "./services.css";
+
+function Services() {
+    return(
+        <section className="services-section">
+            asdfsdafasdfasdfsadfas
+        </section>
+    );
+}
+
+export default Services;

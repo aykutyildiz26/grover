02ebb2cc50d99aa88f08d35e1718f0cd9230cd3e
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-01-13 18:28:28 +0300

    [AY-1] - Layout

--- vite-grover/src/App.jsx
+++ vite-grover/src/App.jsx
@@ -1,20 +1,21 @@
-import  { useState} from "react";
+import { useState } from "react";
 import Header from "./components/navbar/header.jsx";
 import * as images from "./image-paths.js";
 import "./App.css";
 import Hero from "./components/heroSection/hero.jsx";
 import Services from "./components/servicesSection/services.jsx";
+import Features1 from "./components/features1/features1.jsx";
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
       <main>
         <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         <Hero />
-        <Services />   
+        <Services />
+        <Features1 />
       </main>
     </>
   );
